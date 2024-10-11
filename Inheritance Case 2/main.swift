1dbf1c506b5badf69c55a31adc877e7d79c7f0dc
Joseph Mary Ganesan <josephmaryganesan9@gmail.com>
2024-10-11 16:45:26 -0400

    Inheritance case 2 with result

--- Inheritance Case 2/main.swift
+++ Inheritance Case 2/main.swift
@@ -1,11 +1,47 @@
-//
-//  main.swift
-//  Inheritance Case 2
-//
-//  Created by Srikanth.Ganesan.a1 on 2024-10-11.
-//
 
-import Foundation
 
-print("Hello, World!")
+class Child {
+    
+    var childsName : String
+    
+    init() {
+        self.childsName = "Joseph Mary Ganesan"
+        print("Child init()")
+    }
+}
 
+class Mother : Child {
+    
+    var mothersName : String
+    
+    init(mothersName : String){
+        
+        self.mothersName = mothersName
+        print("In new init()")
+        
+    }
+    
+    override init() {
+        print("In override init()")
+        self.mothersName = "Mama Mary"
+    }
+    
+}
+
+var mother1 : Mother = Mother()
+var mother2 : Mother = Mother(mothersName: "Mother Mary")
+print(mother1.mothersName)
+print(mother2.mothersName)
+
+
+/* Result
+ ---------
+ In override init()
+ Child init()
+ In new init()
+ Child init()
+ Mama Mary
+ Mother Mary
+ Program ended with exit code: 0
+
+ */
